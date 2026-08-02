02b91722b4b8f04f48aad4090a0bf039be7ef252
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-02 20:41:25 -0300

    feat(journey): export journey schema

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./client";
 export * from "./schema/core";
+export * from "./schema/journey";
 export * from "./repositories/drawing-zero";
 export * from "./repositories/file-assets";
