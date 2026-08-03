f35b3a9a42de254ae6e24f775d57d8b4268cd900
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-03 14:08:24 -0300

    feat(privacy): pass consent version into join flow

--- apps/web/app/join/[code]/page.tsx
+++ apps/web/app/join/[code]/page.tsx
@@ -1,8 +1,9 @@
+import { ALPHA_CONSENT_VERSION } from "@swd/database";
 import { JoinInvite } from "./JoinInvite";
 
 export const dynamic = "force-dynamic";
 
 export default async function JoinPage({ params }: { params: Promise<{ code: string }> }) {
   const { code } = await params;
-  return <main className="flow-shell"><JoinInvite code={code} /></main>;
+  return <main className="flow-shell"><JoinInvite code={code} consentVersion={ALPHA_CONSENT_VERSION} /></main>;
 }
