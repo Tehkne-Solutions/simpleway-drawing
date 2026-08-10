6c64f00dc6be68c93918b91b96965e283fdbbc78
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-10 17:26:58 -0300

    feat: activate progress surfaces for Visual V1

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -14,6 +14,7 @@ import "./visual-v1.css";
 import "./learn/lesson-v1.css";
 import "./labs-v1.css";
 import "./visual-v1-create-journey.css";
+import "./visual-v1-progress.css";
 import { AppShell } from "./app-shell";
 
 export const metadata: Metadata = {
